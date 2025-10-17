e9e9085bb90336be77c0d7e512c68e56a60daf17
Prateek Agrawal <prateek.bka@gmail.com>
2025-10-17 18:36:47 +0530

    add logger

--- server/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-config();
-
-const { PORT, NODE_ENV } = process.env;
-
-export const Config = {
-  PORT,
-  NODE_ENV
-};
---
+++ server/src/config/logger.ts
@@ -0,0 +1,23 @@
+import winston from "winston";
+import { config } from "./config.js";
+
+const logger = winston.createLogger({
+  level: "info",
+  defaultMeta: { serviceName: "backend" },
+  transports: [
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "combined.log",
+      level: "info",
+      format: winston.format.combine(winston.format.errors({ stack: true }), winston.format.timestamp(), winston.format.json()),
+      silent: config.NODE_ENV === "test"
+    }),
+
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(winston.format.errors({ stack: true }), winston.format.timestamp(), winston.format.json())
+    })
+  ]
+});
+
+export default logger;
