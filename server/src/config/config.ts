c009c32de7bfc6b0d04a8ec4416bd13d3fccb6af
Prateek Agrawal <prateek.bka@gmail.com>
2025-10-18 17:38:24 +0530

    update env config

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_ENV,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
