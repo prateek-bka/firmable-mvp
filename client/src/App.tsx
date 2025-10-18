7ceeead508535d88d63c8081c4e559d9de6447c7
Prateek Agrawal <prateek.bka@gmail.com>
2025-10-18 16:43:50 +0530

    initial client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Button>Button</Button>;
+}
+
+export default App;
