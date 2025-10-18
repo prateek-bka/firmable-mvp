81bd8149f340581a78b6f6484e0a788dac194e98
Prateek Agrawal <prateek.bka@gmail.com>
2025-10-18 17:37:17 +0530

    add router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <>
+    <RouterProvider router={router} />
+    <Toaster
+      position="top-right"
+      richColors
+      expand={false}
+      closeButton
+      duration={4000}
+    />
+  </>
+);
